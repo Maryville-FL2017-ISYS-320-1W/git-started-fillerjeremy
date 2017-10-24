2fecab37e46a2c7658070bceb76b0cd4130ccdc8
fillerjeremy <33054901+fillerjeremy@users.noreply.github.com>
2017-10-24 02:42:19 -0700

    Assignment Complete
    Filled in required parts.

--- src/AboutYou.java
+++ src/AboutYou.java
@@ -4,10 +4,10 @@ public class AboutYou {
 	public static void main(String[] args) {
 		// TODO Modify the values in the String literals in ALL_CAPS below
 		System.out.print("Hello, my name is ");
-		System.out.println("YOUR_NAME");
+		System.out.println("Jeremy");
 
 		System.out.print("And I'm taking this class because ");
-		System.out.println("BECAUSE_WHY");
+		System.out.println("Java is very necessary for me to learn in a career in cyber security.");
 	}
 
 }
